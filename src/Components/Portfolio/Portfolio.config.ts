cf4cea30ed7b8b3c3cb538db8bd65715441fdf7c
Grigory Druzhenkov <druzhenkovgrigory@simple-it.pro>
2025-01-19 16:58:03 +0500

    Добавил портфолио

--- src/Components/Portfolio/Portfolio.config.ts
+++ src/Components/Portfolio/Portfolio.config.ts
@@ -4,34 +4,34 @@ export const projectsConfig = [
         description:
             "An innovative digital robotic device for monitoring and maintaining power lines in real time, improving the efficiency of power grids.",
         link: "https://cablewalker.com/",
-        src: "../../../public/Pictires/Projects/CableWalkerMain.png",
+        src: "/Pictires/Projects/CableWalkerMain.png", // Путь от корня сайта
     },
     {
         name: "CableWalker CRM system",
         description:
             "System for tracking project readiness, the state of power line spans and an employee’s personal account.",
         link: null,
-        src: "../../../public/Pictires/Projects/CableWalkerCRM.png",
+        src: "/Pictires/Projects/CableWalkerCRM.png",
     },
     {
         name: "CC «OLYMP»",
         description:
             "Construction company engaged in the construction of country houses and the selection of sites. Created landing.",
         link: null,
-        src: "../../../public/Pictires/Projects/Olimp.png",
+        src: "/Pictires/Projects/Olimp.png",
     },
     {
         name: "Kampus",
         description:
             "A project that helps students develop, a personal account of trustees has been created to track statistics.",
         link: null,
-        src: "../../../public/Pictires/Projects/Campus.png",
+        src: "/Pictires/Projects/Campus.png",
     },
     {
         name: "KILLNOISE",
         description:
             "A company that deals with soundproofing of premises. Created landing page.",
         link: "https://www.klllnolse.ru/",
-        src: "../../../public/Pictires/Projects/killnoise.png",
+        src: "/Pictires/Projects/killnoise.png",
     },
 ];
